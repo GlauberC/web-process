8baabcc5ef4bb5611f37ada9a4f72a4053cf3226
GlauberC <glaubercarv@gmail.com>
2019-03-28 13:44:53 -0300

    svg fixed

---
+++ public/js/size.js
@@ -0,0 +1,7 @@
+var margin = {top: 20, right: 120, bottom: 20, left: 120},
+    width = 960 - margin.right - margin.left,
+    height = 500 - margin.top - margin.bottom;
+
+
+var tree = d3.layout.tree()
+    .size([height, width]);
