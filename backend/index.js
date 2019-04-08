ce238b0cf2b9080579cfc0b839c40aa7d22a6a2c
GlauberC <glaubercarv@gmail.com>
2019-04-08 11:40:29 -0300

    init react frontend

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 })
 require('./src/controllers/maudeController')(app)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server is running: localhost:${PORT}`))
 
 module.exports = app
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <div className = 'container'>
+          <p>Hello, world</p>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default App;
